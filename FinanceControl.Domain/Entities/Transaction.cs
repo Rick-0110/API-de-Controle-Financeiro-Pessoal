af571a5c764598d8efe0ad475b4b432d9ad37ecd
Henrique <matosh1604@gmail.com>
2025-12-27 12:48:27 -0300

    Erro na requisição do CreateTransaction

--- FinanceControl.Domain/Entities/Transaction.cs
+++ FinanceControl.Domain/Entities/Transaction.cs
@@ -1,23 +1,28 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization; 
 
 namespace FinanceControl.Domain.Entities
 {
     public class Transaction
     {
         public int Id { get; set; }
+
+        
         public string Tittle { get; set; }
+
         public decimal Amount { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
-
-        public TransactionType Type { get; set; }
+        public int Type { get; set; }
 
         public int UserId { get; set; }
 
         [JsonIgnore]
+        [Required]
         public User User { get; set; } = null!;
 
         public int CategoryId { get; set; }
 
+        [JsonIgnore]
         public Category Category { get; set; } = null!;
     }
 }
