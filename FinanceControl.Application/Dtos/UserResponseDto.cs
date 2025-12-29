21d82d7c25f71a409310d10e81e27045b8f95e74
Henrique <matosh1604@gmail.com>
2025-12-29 16:08:47 -0300

    Implementando DTOs de Usuario e corrigindo rota de criação

---
+++ FinanceControl.Application/Dtos/UserResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceControl.Application.Dtos
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+
+}
